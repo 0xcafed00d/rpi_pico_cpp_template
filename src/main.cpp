1bbf7ee21aac4fc59c386d999873741881d2aa33
Lee Witek <5556121+0xcafed00d@users.noreply.github.com>
2022-07-12 14:19:40 +0100

    updated to support pico w

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,52 @@
 
 #include <stdio.h>
-#include "pico/binary_info.h"
-#include "pico/stdlib.h"
+
+#include <pico/binary_info.h>
+#include <pico/stdlib.h>
+
+#ifdef RASPBERRYPI_PICO_W
+#include <pico/cyw43_arch.h>
+#endif
+
+#if defined(PICO_DEFAULT_LED_PIN)
+#warning using built in LED
+void init_led() {
+	gpio_init(PICO_DEFAULT_LED_PIN);
+	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+}
+
+void set_led(bool state) {
+	gpio_put(PICO_DEFAULT_LED_PIN, state);
+}
+#elif defined(RASPBERRYPI_PICO_W)
+#warning using picow LED
+
+void init_led() {
+	cyw43_arch_init();
+}
+
+void set_led(bool state) {
+	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, state);
+}
+#else
+#warning no LED available
+void init_led() {
+}
+
+void set_led(bool state) {
+}
+#endif
 
 int main() {
 	bi_decl(bi_program_description("template raspberry pi pico c++ project"));
 
 	stdio_init_all();
 
-	const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-
-	gpio_init(LED_PIN);
-	gpio_set_dir(LED_PIN, GPIO_OUT);
-
+	init_led();
 	uint32_t count = 1;
 
 	while (true) {
-		gpio_put(LED_PIN, count & 1);
+		set_led(count & 1);
 		printf("%d\n", count);
 		sleep_ms(100);
 		count++;

df53d383d2bc81e3449742a26fecb0c6aeeaef5b
Lee Witek <5556121+0xcafed00d@users.noreply.github.com>
2022-07-14 23:13:56 +0100

    moved led code to seperate file

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,12 @@
-
 #include <stdio.h>
 
-#include <pico/binary_info.h>
 #include <pico/stdlib.h>
+#include <pico/binary_info.h>
 
-#ifdef RASPBERRYPI_PICO_W
-#include <pico/cyw43_arch.h>
-#endif
-
-#if defined(PICO_DEFAULT_LED_PIN)
-#warning using built in LED
-void init_led() {
-	gpio_init(PICO_DEFAULT_LED_PIN);
-	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-}
-
-void set_led(bool state) {
-	gpio_put(PICO_DEFAULT_LED_PIN, state);
-}
-#elif defined(RASPBERRYPI_PICO_W)
-#warning using picow LED
-
-void init_led() {
-	cyw43_arch_init();
-}
-
-void set_led(bool state) {
-	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, state);
-}
-#else
-#warning no LED available
-void init_led() {
-}
-
-void set_led(bool state) {
-}
-#endif
+#include "led.h"
 
 int main() {
-	bi_decl(bi_program_description("template raspberry pi pico c++ project"));
+	bi_decl(bi_program_description("template raspberry pi pico/w c++ project"));
 
 	stdio_init_all();
 
